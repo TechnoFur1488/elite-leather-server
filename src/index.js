46f18954a3c7f9d2996a905bfea878e407c22ac7
Nikita <nikitaloginovxxx1986@gmail.com>
2025-06-28 17:39:28 +0300

    update

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(express.static(path.resolve(__dirname, "..", "static")))
 app.use(fileUpload())
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://elite-leather-client-cjwvp3hfw-nikitas-projects-e30fe775.vercel.app/", "https://elite-leather-client.vercel.app/", "https://elite-leather-client-cjwvp3hfw-nikitas-projects-e30fe775.vercel.app/"],
     credentials: true,
     methods: ["POST", "GET", "PUT", "DELETE"]
 }))
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId, role, res) => {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "strict",
-        secure: false
+        secure: true
     })
 
     return token
